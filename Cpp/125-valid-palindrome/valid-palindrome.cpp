5dc41b0b51dea751074cbfcbd0ce43160a5120ec
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2025-07-10 04:09:07 +0800

    Time: 38 ms (6.76%) | Memory: 10.3 MB (28.17%) - LeetSync

--- Cpp/125-valid-palindrome/valid-palindrome.cpp
+++ Cpp/125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (int i = 0; i < s.length(); i++) {
+        string reverse;
+        for (int i = s.length() - 1; i >= 0; i--) {
             if (isalnum(s[i])) {
-                if (!islower(s[i])) {
-                    s[i] = tolower(s[i]);
-                }
+                s[i] = tolower(s[i]);
+                reverse += s[i];
             }
             else {
                 s.erase(i, 1);
-                i--;
             }
-        }
-        string reverse;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            reverse += s[i];
         }
 
         return s == reverse;
