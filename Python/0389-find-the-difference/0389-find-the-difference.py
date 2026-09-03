c524a61db3efdb35bb789f03956c0e1a0dde696a
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-03 18:55:54 +0800

    Time: 2 ms (72.61%), Space: 12.4 MB (67.87%) - LeetHub

---
+++ Python/0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+
+        for c in t:
+            if t.count(c) > s.count(c):
+                return c
+
+        
+        
