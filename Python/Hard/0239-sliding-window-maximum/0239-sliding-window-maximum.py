4605ee4226f367a5f19ff52f7b9956893243acb8
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-06 12:21:45 +0800

    Time: 255 ms (89.96%), Space: 27.3 MB (98.56%) - LeetHub

---
+++ Python/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        q = collections.deque()
+        res = []
+
+        for i, num in enumerate(nums):
+            if q and q[0] < i - k + 1:
+                q.popleft()
+
+            while q and nums[q[-1]] < num:
+                q.pop()
+
+            q.append(i)
+
+            if i >= k - 1:
+                res.append(nums[q[0]])
+
+        return res
