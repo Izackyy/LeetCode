400da87172cb458f892bfa3a4e931fa3d7547b2b
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-07 22:02:19 +0800

    Time: 28 ms (23.58%), Space: 18.3 MB (9.34%) - LeetHub

---
+++ Python/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = collections.defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            
+            ans[tuple(count)].append(word)
+
+        return list(ans.values())
