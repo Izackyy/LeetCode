fe4d490ec04a4f139f2652b97a6bedc4c3d0725c
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-12 22:14:03 +0800

    Time: 0 ms (100%), Space: 12.3 MB (90.97%) - LeetHub

---
+++ Python/Medium/0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        numerator = math.factorial(m + n - 2)
+        denominator = math.factorial(m - 1) * math.factorial(n - 1)
+
+        return numerator / denominator
