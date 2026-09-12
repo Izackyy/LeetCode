b6e11d85194de22f57c61aadecab2141d867b06b
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-12 22:37:07 +0800

    Time: 0 ms (100%), Space: 12.3 MB (90.97%) - LeetHub

--- Python/Medium/0062-unique-paths/0062-unique-paths.py
+++ Python/Medium/0062-unique-paths/0062-unique-paths.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[1] * n for _ in range(m)]
+        dp = [1] * n
 
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                dp[j] += dp[j - 1]
 
-        return dp[-1][-1]
+        return dp[-1]
