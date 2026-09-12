f11321556ea34dacd7e46382dfa310f49eedbffe
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-12 16:40:35 +0800

    Time: 11 ms (55.79%), Space: 14.1 MB (71.8%) - LeetHub

---
+++ Python/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        freq = Counter(nums)
+        return [item[0] for item in freq.most_common(k)]
