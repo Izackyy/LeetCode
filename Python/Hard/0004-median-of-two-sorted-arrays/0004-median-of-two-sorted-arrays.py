83af097cd6aa9ea2c8f6ede73a8ce1d1ffc19851
Isaac Wong <163094116+Izackyy@users.noreply.github.com>
2026-09-13 18:40:55 +0800

    Time: 8 ms (15.46%), Space: 12.6 MB (15.45%) - LeetHub

--- Python/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ Python/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -5,12 +5,30 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        nums = nums1 + nums2
-        nums.sort()
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        x, y = len(nums1), len(nums2)
+        low, high = 0, x
 
-        mid = len(nums) // 2
+        while low <= high:
+            partition_x = (low + high) // 2
+            partition_y = (x + y + 1) // 2 - partition_x
+            
+            max_left_x = float('-inf') if partition_x == 0 else nums1[partition_x - 1]
+            min_right_x = float('inf') if partition_x == x else nums1[partition_x]
 
-        if len(nums) % 2: 
-            return float(nums[mid])
-        else:
-            return (nums[mid - 1] + nums[mid]) / 2.0
+            max_left_y = float('-inf') if partition_y == 0 else nums2[partition_y - 1]
+            min_right_y = float('inf') if partition_y == y else nums2[partition_y]
+
+            if max_left_x <= min_right_y and max_left_y <= min_right_x:
+                if (x + y) % 2:
+                    return float(max(max_left_x, max_left_y))
+                else:
+                    return (max(max_left_x, max_left_y) + min(min_right_x, min_right_y)) / 2.0
+            
+            elif max_left_x > min_right_y:
+                high = partition_x - 1
+            else:
+                low = partition_x + 1
+                
+                        
